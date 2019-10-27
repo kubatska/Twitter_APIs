b6237684cf77903c261b47e48bc2360ddb3be965
kubatska <47230894+kubatska@users.noreply.github.com>
2019-10-27 18:55:54 +0200

    Update hidden.py

--- hidden.py
+++ hidden.py
@@ -3,14 +3,10 @@
 # https://apps.twitter.com/
 # Create new App and get the four strings
 
-# def oauth():
-#     return {"consumer_key": "s5J7ZV255BieTNpBhouq1FySm",
-#             "consumer_secret": "WixS298B8v8zxtG9VKD5DOwNswIci61eDY3K8faAvnw3ZSkZD1",
-#             "token_key": "1095571267191799809-qod5noNRR45bAX0InfFH85Yuba9gi8",
-#             "token_secret": "HcAVA6HwSnk7Tn4c4zcPfPikLE5xXJmZlK6SGh7Tqa8kF"}
+
 
 def oauth():
-    return {"consumer_key": "s5J7ZV255BieTNpBhouq1FySm",
-            "consumer_secret": "WixS298B8v8zxtG9VKD5DOwNswIci61eDY3K8faAvnw3ZSkZD1",
-            "token_key": "1095571267191799809-qod5noNRR45bAX0InfFH85Yuba9gi8",
-            "token_secret": "HcAVA6HwSnk7Tn4c4zcPfPikLE5xXJmZlK6SGh7Tqa8kF"}
+    return {"consumer_key": "",
+            "consumer_secret": "",
+            "token_key": "",
+            "token_secret": ""}
